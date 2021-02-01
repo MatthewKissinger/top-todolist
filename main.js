918ec82af8b47629dcc0dd651046ff54874f705d
Matt Kissinger <matthew.t.kissinger@gmail.com>
2021-02-01 15:42:23 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+
+console.log('where to begin?');
+
+
+// cacheDOM
+const projectList = document.querySelector('#projectList');
+const projectDisplay = document.querySelector('#projectDisplay');
+const toDoInfo = document.querySelector('#toDoInfo');

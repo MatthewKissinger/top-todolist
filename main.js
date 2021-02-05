80b6844bac8372424e6f434b54379cfba59107f2
Matt Kissinger <matthew.t.kissinger@gmail.com>
2021-02-04 18:11:54 -0800

    added completed array functionality

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // To DO list for the To do app lol
-// 1) Update the template to have DOM element variables -- DONE!
-//  a) add the class to change the color of the star for the given toDo list's priority -- DONE!
-// 2) add a function to toggle the toDo list's priority on the star button -- DONE!
-//  a) needs to update the object's priority status in projectsArrays -- DONE!
-//  b) delegate this method by using 'closest' method -- research -- DONE!
-// 3) create a button on the todo List items that will place them in a completed array for each project
+
+// 1) create a button on the todo List items that will place them in a completed array for each project -- DONE!
+//  a) add a delegated event lister to push the toDo item into the completed array -- DONE!
+//  b) create a completedArray container below the toDoArray container -- DONE!
+//  c) render the completedArray items into the container -- DONE!
+// 2) create an event listener on the completed header div,  it will display or hide the completed container when clicked
 
 
 // cacheDOM for the entire App
@@ -14,6 +14,8 @@ const toDoInfo = document.querySelector('#to-do-info');
 
 // Project Storage
 
+
+// default project created for demo, testing, and styling purposes
 let projectsArray = [
     {
         name: "Default", 
@@ -33,29 +35,104 @@ let projectsArray = [
                 checklist: ""
             }
         ],
-        completedArray: []
+        completedArray: [
+            {
+                title: "Workout", 
+                dueDate: "", 
+                priority: "notImportant", 
+                notes: "", 
+                checklist: ""
+            }
+        ]
     }
 ];
 
+// input toDo Module
 
-// Project Display Module
+let input = (function() {
 
 // cacheDOM
 const projectName = document.querySelector('#project-name');
 let currentProject = projectName.innerText;
+// public variable
 let index = projectsArray.findIndex(x => x.name === currentProject);
 
 const inputToDo = document.querySelector('#add-to-do');
 let plusButton = document.querySelector('#plus-button');
 let importantInput = document.querySelector('#important-input');
 
-const toDoContainer = document.querySelector('#to-do-container');
-
 // bind events
 inputToDo.addEventListener('keydown', addToDo);
 plusButton.addEventListener('click', addToDo);
 importantInput.addEventListener('click', importantInputToggle);
-toDoContainer.addEventListener('click', toDoImportantToggle);
+
+function addToDo(e) {
+    if (13 == e.keyCode || e.target.matches('svg.fa-plus') || e.target.matches('path')) {
+        let name = inputToDo.value;
+        if (name === '') {
+            return;
+        } else {
+            let dueDate = '';
+            let priority = '';
+            if (importantInput.classList.contains('important')) {
+                priority = 'important';
+
+            } else {
+                priority = 'notImportant';
+            }
+            let notes = '';
+            let checklist = '';
+            let newToDo = new toDoFactory(name, dueDate, priority, notes, checklist);
+            projectsArray[index]['toDoArray'].push(newToDo);
+            toDoList.rendertoDoList();
+            inputToDo.value = '';
+            importantInputReset();
+        }
+    }
+}
+
+function toDoFactory(title, dueDate, priority, notes, checklist) {
+    return {
+        title, dueDate, priority, notes, checklist
+    }
+};
+
+function importantInputToggle(e) {
+    if (e.target.matches('svg.fa-star') || e.target.matches('path')) {
+        if (importantInput.classList.contains('not-important')) {
+            importantInput.classList.remove('not-important');
+            importantInput.classList.add('important');
+        } else {
+            importantInput.classList.add('not-important');
+            importantInput.classList.remove('important');
+        }      
+    }
+}
+
+function importantInputReset() {
+    if (importantInput.classList.contains('not-important')) {
+        return
+    } else {
+        importantInput.classList.add('not-important');
+        importantInput.classList.remove('important');
+    }
+}
+
+return {
+    index: index
+}
+
+})();
+
+// toDoList Module -- displays each itme in the toDoArray for for the particular project
+
+let toDoList = (function() {
+
+// cache DOM
+const toDoContainer = document.querySelector('#to-do-container');
+
+// bind events
+toDoContainer.addEventListener('click', toDoDelegation);
 
 // render function for the toDo container
 renderToDoList();
@@ -63,7 +140,7 @@ renderToDoList();
 // render function for toDo lists items
 function renderToDoList() {
     toDoContainer.innerHTML = '';
-    projectsArray[index].toDoArray.forEach(item => {
+    projectsArray[input.index].toDoArray.forEach(item => {
         toDoContainer.appendChild(toDoTemplate(item['title'], item['priority']));
     }); 
 }
@@ -94,74 +171,86 @@ function toDoTemplate(title, priority) {
     return toDoWrapper;
 };
 
-function toDoFactory(title, dueDate, priority, notes, checklist) {
-    return {
-        title, dueDate, priority, notes, checklist
-    }
-};
-
-function importantInputToggle(e) {
-    if (e.target.matches('svg.fa-star') || e.target.matches('path')) {
-        if (importantInput.classList.contains('not-important')) {
-            importantInput.classList.remove('not-important');
-            importantInput.classList.add('important');
-        } else {
-            importantInput.classList.add('not-important');
-            importantInput.classList.remove('important');
-        }      
-    }
-}
-
-function importantInputReset() {
-    if (importantInput.classList.contains('not-important')) {
-        return
-    } else {
-        importantInput.classList.add('not-important');
-        importantInput.classList.remove('important');
-    }
-}
-
-function toDoImportantToggle(e) {
+// Delegates for the important Star and checkmark Completed icons
+function toDoDelegation(e) {
     const target = e.target;
-    if (e.target.matches('svg.fa-star') || e.target.matches('path')) {
-        let closest = target.closest('div');
+    let targetDValue = target.getAttribute('d');
+
+    let starDValue = 'M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z';
+    let checkDValue = 'M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z';
+    
+    let closest = target.closest('div');
+  
+    // this first conditional is to eliminate an uncaught type error
+    if (closest.querySelector('.to-do-card') === null) {
+        return;
+    } else {
         let toDoTitle = closest.querySelector('.to-do-card').innerText;
-        let toDoIndex = projectsArray[index]['toDoArray'].findIndex(x => x.title === toDoTitle);
-        let toDoPriority = projectsArray[index]['toDoArray'][toDoIndex]['priority'];
+        let toDoIndex = projectsArray[input.index]['toDoArray'].findIndex(x => x.title === toDoTitle);
+
+        // important toggle delegation
+        if (target.matches('svg.fa-star') || starDValue === targetDValue ) {
+            let toDoPriority = projectsArray[input.index]['toDoArray'][toDoIndex]['priority'];
         
         if (toDoPriority === 'important') {
-            projectsArray[index]['toDoArray'][toDoIndex]['priority'] = 'notImportant';
+            projectsArray[input.index]['toDoArray'][toDoIndex]['priority'] = 'notImportant';
         } else if (toDoPriority === 'notImportant') {
-            projectsArray[index]['toDoArray'][toDoIndex]['priority'] = 'important';
+            projectsArray[input.index]['toDoArray'][toDoIndex]['priority'] = 'important';
         }
-    }   
-    renderToDoList();
+        }   // checkmark to completed Array delegation
+            else if (target.matches('svg.fa-check') || checkDValue === targetDValue) {
+                projectsArray[input.index]['completedArray'].push(projectsArray[input.index]['toDoArray'][toDoIndex]);
+                projectsArray[input.index]['toDoArray'].splice(toDoIndex, 1);
+        } 
+    renderToDoList();   
+    completedList.renderCompletedArray();
+    }    
 }
 
-function addToDo(e) {
-    if (13 == e.keyCode || e.target.matches('svg.fa-plus') || e.target.matches('path')) {
-        let name = inputToDo.value;
-        if (name === '') {
-            return;
-        } else {
-            let dueDate = '';
-            let priority = '';
-            if (importantInput.classList.contains('important')) {
-                priority = 'important';
-
-            } else {
-                priority = 'notImportant';
-            }
-            let notes = '';
-            let checklist = '';
-            let newToDo = new toDoFactory(name, dueDate, priority, notes, checklist);
-            projectsArray[index]['toDoArray'].push(newToDo);
-            renderToDoList();
-            inputToDo.value = '';
-            importantInputReset();
-        }
-    }
+return {
+    rendertoDoList: renderToDoList,
 }
+
+})();
+
+
+// completed Array module -- render function and toggle incomplete 
+let completedList = (function() {
+
+//cache DOM
+let completedContainer = document.querySelector('#completed-container');
+
+renderCompletedArray();
+
+function renderCompletedArray() {
+    completedContainer.innerHTML = '';
+    projectsArray[input.index].completedArray.forEach(item => {
+        completedContainer.appendChild(template(item['title']));
+    }); 
+}
+
+function template(title) {
+    let wrapper = document.createElement('div');
+    wrapper.classList.add('to-do-wrapper');
+
+    let toDoCard = document.createElement('div');
+    toDoCard.classList.add('to-do-card');
+    toDoCard.innerText = `${title}`;
+
+    wrapper.appendChild(toDoCard);
+
+    return wrapper;
+}
+
+return {
+    renderCompletedArray: renderCompletedArray
+}
+
+})();
+
+
+
+
 
 
 
